3fd4402bf5bf999cd1f1fc5367112fef371ccb19
arcreaneDev <dominique@arcreane.com>
2021-07-06 17:07:42 +0200

    First step in creating a Game Can select category and difficulty in the main

--- Memory/Game.cs
+++ Memory/Game.cs
@@ -8,5 +8,14 @@ namespace Memory
 {
     class Game
     {
+        private Player m_Player;
+        private Category m_Category;
+        private Difficulty m_Difficulty;
+
+        public Game(Category p_Cat, Difficulty p_Diff)
+        {
+            m_Category = p_Cat;
+            m_Difficulty = p_Diff;
+        }
     }
 }
--- Memory/Program.cs
+++ Memory/Program.cs
@@ -6,7 +6,21 @@ namespace Memory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Category cat = (Category)GetEnumSelection(nameof(Category), typeof(Category));
+            Difficulty diff = (Difficulty)GetEnumSelection(nameof(Difficulty), typeof(Difficulty));
+
+            Console.WriteLine($"You selected {cat} and {diff}");
+        }
+
+        private static int GetEnumSelection(String enumName, Type enumType)
+        {
+            Console.WriteLine($"To select {enumName} type : ");
+            foreach (var item in Enum.GetValues(enumType))
+            {
+                Console.WriteLine($"{(int)item} - {item}");
+            }
+
+            return int.Parse(Console.ReadLine());
         }
     }
 }
